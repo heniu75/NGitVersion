e20cf5fa3b4207bc632da7745bf61b66cdd5a16e
Heinrich van Vught <heinrich.van.vught@gmail.com>
2017-04-22 10:16:38 +0100

    Updated with long hash and company info

--- src/NGitVersion/Model/Model.cs
+++ src/NGitVersion/Model/Model.cs
@@ -11,6 +11,7 @@ namespace NGitVersion.Model
 
         private readonly Lazy<string> mRevision;
         private readonly Lazy<string> mShortHash;
+        private readonly Lazy<string> mHash;
         private readonly Lazy<string> mBranch;
         private readonly Lazy<string> mHasLocalChange;
         private readonly string mBuildConfig;
@@ -21,6 +22,7 @@ namespace NGitVersion.Model
 
             mRevision = new Lazy<string>(() => mRepository.Commits.Count().ToString(CultureInfo.InvariantCulture));
             mShortHash = new Lazy<string>(() => mRepository.Commits.First().Sha.Substring(0, 7));
+            mHash = new Lazy<string>(() => mRepository.Commits.First().Sha);
             mBranch = new Lazy<string>(() => mRepository.Head.CanonicalName);
             mHasLocalChange = new Lazy<string>(() => mRepository.RetrieveStatus().IsDirty.ToString(CultureInfo.InvariantCulture));
 #if DEBUG
@@ -30,18 +32,19 @@ namespace NGitVersion.Model
 #endif
         }
 
-        public string Company        { get { return "TODO Company"; } }
-        public string Product        { get { return "TODO Product"; } }
-        public string Copyright      { get { return "TODO Copyright"; } }
-        public string Trademark      { get { return "TODO Trademark"; } }
+        public string Company        { get { return "Linkusoft"; } }
+        public string Product        { get { return "Briga"; } }
+        public string Copyright      { get { return "Copyright 2017"; } }
+        public string Trademark      { get { return "Briga TM"; } }
         public string Culture        { get { return ""; } }
 
-        public string Major          { get { return "0"; } } // TODO
+        public string Major          { get { return "1"; } } // TODO
         public string Minor          { get { return "0"; } } // TODO
         public string Build          { get { return "0"; } } // TODO
 
         public string Revision       { get { return mRevision.Value; } }
         public string ShortHash      { get { return mShortHash.Value; } }
+        public string Hash { get { return mHash.Value; } }
         public string Branch         { get { return mBranch.Value; } }
         public string HasLocalChange { get { return mHasLocalChange.Value; } }
         public string BuildConfig    { get { return mBuildConfig; } }
